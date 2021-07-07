7bc2189cf97a496b3ea0003c7d97bb618e6df087
rummolprod999 <rummolprod999@gmail.com>
2021-07-07 19:12:53 +0300

    first

--- src/com/rummolprod999/Main.java
+++ src/com/rummolprod999/Main.java
@@ -5,7 +5,7 @@ import java.util.stream.Stream;
 public class Main {
 
     public static void main(String[] args) {
-	Stream.of("abc", "def").map(Object::toString).forEach(System.out::println);
-    Stream.of("abc", "def").map(String::toString).forEach(System.out::println);
+        Stream.of("abc", "def").map(Object::toString).forEach(System.out::println);
+        Stream.of("abc", "def").map(String::toString).forEach(System.out::println);
     }
 }
